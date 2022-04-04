e07a9890c2b4541ab5b2da67c624af44c75d2a92
박정식 <jstar.park@partner.samsung.com>
2022-04-04 10:12:21 +0900

    initial commit

---
+++ src/routes/map.svelte
@@ -0,0 +1,19 @@
+<script>
+	let container;
+	let map;
+	let zoom = 15;
+    let center = {lat: 37.499528, lng: 127.027309};
+    
+    import { onMount } from 'svelte';
+    
+	onMount(async () => {
+		map = new google.maps.Map(container, {
+            zoom,
+			center,
+			styles: {} // optional
+		});
+	});
+</script>
+
+
+<div bind:this={container} style="width: 100%;height: 900px;"></div>
