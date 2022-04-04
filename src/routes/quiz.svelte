c83a291aeb3b0c60b92c1e5b04e8f337631b79b9
박정식 <jstar.park@partner.samsung.com>
2022-04-04 13:45:40 +0900

    2st

--- src/routes/quiz.svelte
+++ src/routes/quiz.svelte
@@ -1,15 +1,65 @@
 <script>
-  import {goto} from ['$app/navigation']
+  import {goto} from '$app/navigation'
+  import {score, quizList} from '$lib/store.js'
 
   let isRunning = false
 
+  let list = $quizList
+  let idx = -1
+  let current = null
+  let bonus = 100
+  $score = 0
+  let timer;
+  let kolor = {
+    red: '빨강',
+    orandge: '주황',
+    yellow: '노랑',
+    purple: '보라',
+    green: '초록',
+    black: '검정',
+    blue: '파랑',
+    brown: '갈색',
+    gray: '회색',
+    pink: '분홍'
+  }
+
   function start() {
+    list.sort(() => Math.random()-0.5)
     alert('Ready?')
+    $score = 0
+    next()
     isRunning = true
   }
 
+  function next() {
+    idx = idx + 1
+    if(idx == list.length) {
+      stop()
+      return1
+    }
+    current = list[idx]
+    current.choice.sort(() => Math.random()-0.5)
+    current = current
+    resetProgress()
+  }
+
+  function resetProgress() {
+    clearInterval(timer)
+    bonus = 100
+    timer = setInterval(() => {
+      bonus = bonus - 1
+      if(bonus == 0) {
+        clearInterval(timer)
+      }
+    }, 40)
+  }
+
   function stop() {
+    idx = -1
+    clearInterval(timer)
+    current = null
     isRunning = false
+    alert(`수고하셨어요. 총 점수는 {$score}입니다`)
     goto('/save')
   }
 
@@ -35,8 +85,57 @@
   {#if isRunning}
     <button style="width: 45%; height: 60px; font-weight: bold; font-size: 30px;" on:click={stop}>그만하기</button>
   {:else}
+    <p>총 점수: {$score}</p>
+    <div style="width: 100%">
+      <progress style="width: 100%" value={bonus} max=100 />
+    </div>
+    <p></p>
+    <p>{idx + 1} 번 문제! </p>
+
+    {#if idx%2 == 0}
+      <div style="color: {current.question.color}; background-color: white;" class="question">
+        {kolor[current.question.text]}
+      </div>
+
+      {#each current.choice as item}
+        <button style="color: {item.color}; background-color: white;" class="choice" on:click={() => {
+          $score = score + (current.question.text == item.color ? 10 + bonus : 0)
+          next()
+        }}>{kolor[item.text]}
+        </button>
+      {/each}
+    {:else}
+      <div style="color: white; background-clolor: {current.question.color}" class='question'>{kolor[current.question.text]}</div>
+      <p></p>
+      
+      {#each current.choice as item}
+        <button style="color: white; background-color: {item.color};" class="choice" on:click={() => {
+          $score = score + (current.question.color == item.text ? 10 + bonus : 0)
+          next()
+        }}>{kolor[item.text]}
+        </button>
+      {/each}
+    {/if}
     <button style="width: 45%; height: 60px; font-weight: bold; font-size: 30px;" on:click={start}>시작</button>
   {/if}
   
   <button style="width: 45%; height: 60px; font-weight: bold; font-size: 30px;" on:click={goHome}>홈으로</button>
 </p>
+
+<style>
+  .question {
+    margin: 0 auto;
+    text-align: center;
+    text-shadow: 1px 1px 1px #000;
+    font-weight: bold;
+    font-size: 60px;
+    width: 150px;
+  }
+  .choice {
+    text-shadow: 1px 1px 1px #000;
+    width: 50%;
+    height: 150px;
+    font-weight: bold;
+    font-size: 50px;
+  }
+</style>
--- src/routes/save.svelte
+++ src/routes/save.svelte
@@ -1,5 +1,23 @@
+<script>
+  import {goto} from '$app/navigation'
+  import {score, scoreList} from '$lib/store.js'
+
+  let name = '--'
+
+  function saveScore() {
+    $scoreList.push({score: $score, name: name})
+    $scoreList.sort((a, b) => b.score - a.score)
+    $scoreList.pop()
+    goto('/')
+  }
+</script>
 <svelte:head>
   <title>점수 저장하기</title>
 </svelte:head>
 
 <h1 style="text-align:center">점수 저장하기</h1>
+
+<h4>이름을 입력하고 저장 버튼을 누르세요.</h4>
+<p>획득점수 : {$score}</p>
+<p>이름 : <input type=text biind:value={name}></p>
+<button on:click={saveScore}>저장</button>
